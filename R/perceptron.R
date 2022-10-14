a60af564b996da28349f2705a7681bcdf0efab55
gongcongcong <991624249@qq.com>
2022-10-14 14:49:17 +0800

    adding predict function and modifying the plot function to use new data

--- R/perceptron.R
+++ R/perceptron.R
@@ -3,18 +3,28 @@
 #' @export
 #' @importFrom R6 R6Class
 #' @examples
-#' #"===============Example One===============\n")
+#' #===============Example One===============
 #' X <- iris[1:100,1:2]
 #' y <- iris$Species[1:100]
 #' m <- Perceptron$new(max_iteration = 10000)
 #' m$fit(X, y)
 #' m$plot()
-#' #"===============Example Two ===============\n")
+#' #===============Example Two ===============
 #' X <- matrix(c(3, 3, 4, 3, 1, 1), ncol = 2, byrow = TRUE)
 #' y <- c(1, 1, -1)
 #' fit <- Perceptron$new()
 #' fit$fit(X, y, w = c(0,0), b = 0)
 #' fit$plot()
+#' #===============Example Three ===============
+#' X <- iris[1:100,1:2]
+#' y <- iris$Species[1:100]
+#' set.seed(123)
+#' idx <- sample(100, 50, replace = FALSE)
+#' m <- Perceptron$new()
+#' m$fit(X[idx, ], y[idx])
+#' m$plot()
+#' m$predict(X[-idx,], y[-idx])
+#' m$plot(X[-idx, ], y[-idx])
 
 Perceptron <- R6Class(
         "Perceptron",
@@ -94,18 +104,22 @@ Perceptron <- R6Class(
                         self$model <- substitute(
                                 w1 * x[1] + w2 * x[2] + b == 0,
                                 list(
-                                        w1 = round(self$w[1], 2),
-                                        w2 = round(self$w[2], 2),
+                                        w1 = round(unname(self$w[1]), 2),
+                                        w2 = round(unname(self$w[2]), 2),
                                         b = round(self$b, 2)
                                 ))
                 },
                 #' @description plot
-                plot = function() {
-                        X <- self$X
-                        y <- self$y
+                #' @param X same as the `X` of `fit`
+                #' @param y same as the `y` of `fit`
+                plot = function(X = NULL, y = NULL) {
+                        X <- ifelse(is.null(X), list(X=self$X), list(X=as.matrix(X)))[[1]]
+                        y <- ifelse(is.null(y), list(y=self$y),
+                                    list(y=ifelse(y == self$labels[[1]],
+                                                  -1, 1)))[[1]]
                         plot(X, col = 'blue', xlab = "", ylab = "")
                         mtext(as.expression(self$model))
-                        points(X[y == 1,], col = "red")
+                        points(subset(X, y == 1), col = "red")
                         abline(-(self$b/self$w[2]), -(self$w[1]/self$w[2]))
                         title(
                               "Perceptron Model",
@@ -114,9 +128,39 @@ Perceptron <- R6Class(
                               ylab = ifelse(is.null(colnames(X)),
                                             expression(x[2]), colnames(X)[2])
                               )
-                        legend("bottomright", legend = self$labels,
-                               col = c("blue", "red"), pch = 1)
+                        pre_y <- self$predict(X) |> as.vector()
+                        pre_y <- ifelse(pre_y == self$labels[[1]], -1, 1)
+                        if (any(pre_y != y)) {
+                                points(X[pre_y != y, ], col = 'orange', pch = 16)
+                                legend("bottomright",
+                                       legend = c(self$labels, "error predict"),
+                                       col = c("blue", "red", "orange"), pch = 1)
+                        } else {
+                                legend("bottomright", legend = self$labels,
+                                       col = c("blue", "red"), pch = 1)
+                        }
+
+
+                },
+                #' @param new_X the new data need to predict
+                #' @param new_y the y value of new data
+                predict = function(new_X, new_y = NULL) {
+                        new_X <- as.matrix(new_X)
+                        y <- ifelse(new_y == self$labels[[1]], -1, 1)
+                        pre_y <- vapply(seq_len(nrow(new_X)),
+                                        \(x) {
+                                                sign(private$.predict(new_X[x, ]))
+                                        }, numeric(1))
+                        pre_y <- factor(pre_y, levels = c(-1, 1), labels = self$labels)
+                        if (!is.null(new_y)) {
+                                print(table(pre_y, new_y))
+                                return(matrix(c(new_y, pre_y), ncol = 2,
+                                              dimnames = list(NULL, c("y", "pred.y"))))
+                        } else {
+                                        return(matrix(pre_y, ncol = 1,
+                                               dimnames = list(NULL, "pred.y")))
+                        }
                 }
-        )
+                )
 )
 
--- README.md
+++ README.md
@@ -46,3 +46,96 @@ fit$plot()
 ```
 
 ![](README_files/figure-gfm/unnamed-chunk-4-1.png)<!-- -->
+
+===============Example Three ===============
+
+``` r
+X <- iris[1:100,1:2]
+y <- iris$Species[1:100]
+set.seed(123)
+idx <- sample(100, 50, replace = FALSE)
+m <- Perceptron$new()
+m$fit(X[idx, ], y[idx])
+```
+
+    ## Initial weights is:  -0.5604756 -0.2301775 
+    ##  Initial intercept is:  1.558708 
+    ## epoch:  573  
+    ##  w:  3.889524 -5.860177  b:  -4.141292
+
+``` r
+m$plot()
+```
+
+![](README_files/figure-gfm/unnamed-chunk-6-1.png)<!-- -->
+
+``` r
+pre <- m$predict(X[-idx,], y[-idx])
+```
+
+    ##             new_y
+    ## pre_y        setosa versicolor virginica
+    ##   setosa         25          2         0
+    ##   versicolor      0         23         0
+
+``` r
+pre
+```
+
+    ##       y            pred.y      
+    ##  [1,] "setosa"     "setosa"    
+    ##  [2,] "setosa"     "setosa"    
+    ##  [3,] "setosa"     "setosa"    
+    ##  [4,] "setosa"     "setosa"    
+    ##  [5,] "setosa"     "setosa"    
+    ##  [6,] "setosa"     "setosa"    
+    ##  [7,] "setosa"     "setosa"    
+    ##  [8,] "setosa"     "setosa"    
+    ##  [9,] "setosa"     "setosa"    
+    ## [10,] "setosa"     "setosa"    
+    ## [11,] "setosa"     "setosa"    
+    ## [12,] "setosa"     "setosa"    
+    ## [13,] "setosa"     "setosa"    
+    ## [14,] "setosa"     "setosa"    
+    ## [15,] "setosa"     "setosa"    
+    ## [16,] "setosa"     "setosa"    
+    ## [17,] "setosa"     "setosa"    
+    ## [18,] "setosa"     "setosa"    
+    ## [19,] "setosa"     "setosa"    
+    ## [20,] "setosa"     "setosa"    
+    ## [21,] "setosa"     "setosa"    
+    ## [22,] "setosa"     "setosa"    
+    ## [23,] "setosa"     "setosa"    
+    ## [24,] "setosa"     "setosa"    
+    ## [25,] "setosa"     "setosa"    
+    ## [26,] "versicolor" "versicolor"
+    ## [27,] "versicolor" "versicolor"
+    ## [28,] "versicolor" "versicolor"
+    ## [29,] "versicolor" "versicolor"
+    ## [30,] "versicolor" "versicolor"
+    ## [31,] "versicolor" "versicolor"
+    ## [32,] "versicolor" "versicolor"
+    ## [33,] "versicolor" "versicolor"
+    ## [34,] "versicolor" "versicolor"
+    ## [35,] "versicolor" "versicolor"
+    ## [36,] "versicolor" "versicolor"
+    ## [37,] "versicolor" "versicolor"
+    ## [38,] "versicolor" "versicolor"
+    ## [39,] "versicolor" "versicolor"
+    ## [40,] "versicolor" "versicolor"
+    ## [41,] "versicolor" "versicolor"
+    ## [42,] "versicolor" "setosa"    
+    ## [43,] "versicolor" "setosa"    
+    ## [44,] "versicolor" "versicolor"
+    ## [45,] "versicolor" "versicolor"
+    ## [46,] "versicolor" "versicolor"
+    ## [47,] "versicolor" "versicolor"
+    ## [48,] "versicolor" "versicolor"
+    ## [49,] "versicolor" "versicolor"
+    ## [50,] "versicolor" "versicolor"
+
+``` r
+m$plot(X[-idx, ], y[-idx])
+```
+
+![](README_files/figure-gfm/unnamed-chunk-8-1.png)<!-- -->
